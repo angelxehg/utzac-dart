2c3d3b2e6c474b03154e60b38fe18341a22a1edf
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-17 19:37:58 -0500

    create mapas.dart

---
+++ mapas.dart
@@ -0,0 +1,8 @@
+void main() {
+  Map alumno = {'name': "ALMO", 'edad': 160, 'grupo': "14 ITIC"};
+  print(alumno['name']);
+  print(alumno['edad']);
+  Map<int, String> equipos = {1: "America", 2: "Cruz Azul", 3: "Chivas"};
+  print(equipos[1]);
+  print(equipos);
+}
