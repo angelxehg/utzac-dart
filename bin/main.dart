8202b221802d2f8f61adb9c67f701ba987d29017
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-17 19:05:31 -0500

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 10;
+  double pi = 3.1416;
+  double stature = 1.74;
+  String name = "Angel";
+  print(number);
+  print("Valor de PI: $pi");
+  print("Mi estatura es: $stature");
+  print("Mi nombre es: $name");
+  print("Mi nombre mide: ${name.length}");
+}
