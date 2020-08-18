5d49dc060d22d4a02e6ac0f086e3177592e9be28
Angel Hurtado <50889225+angelxehg@users.noreply.github.com>
2020-08-17 19:26:11 -0500

    finish listas.dart

--- listas.dart
+++ listas.dart
@@ -1,1 +1,15 @@
-void main() {}
+void main() {
+  List edades = [29, 23, 46, 67, 89, 12, 78, 91];
+  print(edades);
+  List<String> nombres = [
+    "Jesús",
+    "Manuel",
+    "Cristina",
+    "Angel",
+    "Diego",
+    "José"
+  ];
+  print(nombres);
+  edades.add("Salvador");
+  print(edades);
+}
